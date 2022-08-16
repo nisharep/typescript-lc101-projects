b5b695c9c174222dece8f971b89a7d735a3b1ad3
Nisha <rajendran.nisha@gmail.com>
2022-08-15 22:22:26 -0500

    TypeScript studio JS files commit

---
+++ studio/Rocket.js
@@ -0,0 +1,44 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var totalMassKg = 0;
+        for (var i = 0; i < items.length; i++) {
+            totalMassKg = totalMassKg + items[i].massKg;
+        }
+        return totalMassKg;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
+    };
+    Rocket.prototype.canAdd = function (item) {
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
